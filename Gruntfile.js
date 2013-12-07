45d7b49084c351d2a0fda4efbf1e964c4122d4be
Vadim Demedes <vdemedes@gmail.com>
2013-12-07 22:37:33 +0100

    Bower integration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,37 @@
-/*global module:false*/
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        banner: '/*! <%= pkg.title %> - v<%= pkg.version %> by <%= pkg.author.name %> (<%= pkg.author.email %>) - ' +
-                '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      dist: {
-        src: ['lib/history.js', 'src/joconut.js'],
-        dest: 'dist/joconut.js'
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'dist/joconut.min.js': ['dist/joconut.js']
-        }
-      }
-    },
-    watch: {
-      files: ['src/joconut.js'],
-      tasks: ['concat', 'uglify']
-    }
-  });
-  
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.file.mkdir('build');
 
+	grunt.initConfig({
+		pkg: require('./package.json'),
 
-  // Default task.
-  grunt.registerTask('default', ['concat', 'uglify']);
+		concat: {
+			options: {
+				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> by <%= pkg.author.name %> (<%= pkg.author.email %>) - ' +
+				'<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			dist: {
+				src: ['lib/history.js/scripts/bundled-uncompressed/html4+html5/jquery.history.js', 'src/joconut.js'],
+				dest: 'build/joconut.js'
+			}
+		},
 
+		uglify: {
+			dist: {
+				files: {
+					'build/joconut.min.js': ['build/joconut.js']
+				}
+			}
+		},
+
+		watch: {
+			files: ['src/joconut.js'],
+			tasks: ['concat', 'uglify']
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['concat', 'uglify']);
 };
